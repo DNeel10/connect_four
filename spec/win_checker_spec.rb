34712cd8b954f496c64b585c5e7f61556f874352
Danny Neel <dneel10@gmail.com>
2022-12-12 22:55:15 -0500

    add check_horizontal_wins method tests

--- spec/win_checker_spec.rb
+++ spec/win_checker_spec.rb
@@ -13,7 +13,7 @@ describe WinChecker do
       subject(:win_vertical) { described_class.new }
 
       before do 
-        board.grid[0] = ['black', 'black', 'black', 'black', nil, nil]
+        board.grid[3] = ['black', 'black', 'black', 'black', nil, nil]
       end
 
       it 'returns true' do
@@ -31,6 +31,86 @@ describe WinChecker do
 
       it 'returns false' do
         expect(game_vertical.check_vertical_wins(board)).to eq false
+      end
+    end
+  end
+
+  describe '#check_horizontal_wins' do
+    context 'player has 4 markers in a horizontal alignment along the bottom' do
+      let(:board) { instance_double(Board, :grid => Array.new(7) { Array.new(6, nil) }) }
+      subject(:win_horizontal) { described_class.new }
+
+      before do
+        board.grid[0] = ['black', nil, nil, nil, nil, nil, nil]
+        board.grid[1] = ['black', nil, nil, nil, nil, nil, nil]
+        board.grid[2] = ['black', nil, nil, nil, nil, nil, nil]
+        board.grid[3] = ['black', nil, nil, nil, nil, nil, nil]
+      end
+
+      it 'returns true' do
+        expect(win_horizontal.check_horizontal_wins(board)).to eq true
+      end
+    end
+
+    context 'player has 4 markers in a horizontal alignment along the top' do
+      let(:board) { instance_double(Board, :grid => Array.new(7) { Array.new(6, nil) }) }
+      subject(:win_horizontal) { described_class.new }
+
+      before do
+        board.grid[0] = [nil, nil, nil, nil, nil, nil, 'black']
+        board.grid[1] = [nil, nil, nil, nil, nil, nil, 'black']
+        board.grid[2] = [nil, nil, nil, nil, nil, nil, 'black']
+        board.grid[3] = [nil, nil, nil, nil, nil, nil, 'black']
+      end
+
+      it 'returns true' do
+        expect(win_horizontal.check_horizontal_wins(board)).to eq true
+      end
+    end
+
+    context 'player has 4 markers in a horizontal alignment in the middle of the board' do
+      let(:board) { instance_double(Board, :grid => Array.new(7) { Array.new(6, nil) }) }
+      subject(:win_horizontal) { described_class.new }
+
+      before do
+        board.grid[0] = [nil, nil, nil, nil, nil, nil, nil]
+        board.grid[1] = [nil, nil, nil, nil, nil, nil, nil]
+        board.grid[2] = [nil, nil, nil, 'black', nil, nil, nil]
+        board.grid[3] = [nil, nil, nil, 'black', nil, nil, nil]
+        board.grid[4] = [nil, nil, nil, 'black', nil, nil, nil]
+        board.grid[5] = [nil, nil, nil, 'black', nil, nil, nil]
+        board.grid[6] = [nil, nil, nil, nil, nil, nil, nil]
+      end
+
+      it 'returns true' do
+        expect(win_horizontal.check_horizontal_wins(board)).to eq true
+      end
+    end
+
+    context 'player does not have 4 markers in a horizontal alignment' do
+      let(:board) { instance_double(Board, :grid => Array.new(7) { Array.new(6, nil) }) }
+      subject(:win_horizontal) { described_class.new }
+
+      before do
+        board.grid[0] = ['red', nil, nil, nil, nil, nil, nil]
+        board.grid[1] = ['red', nil, nil, nil, nil, nil, nil]
+        board.grid[2] = ['black', nil, nil, nil, nil, nil, nil]
+        board.grid[3] = ['black', nil, nil, nil, nil, nil, nil]
+      end
+
+      it 'returns false' do
+        expect(win_horizontal.check_horizontal_wins(board)).to eq false
+      end
+    end
+  end
+
+  describe '#check_diagonal_wins' do
+    subject(:win_diagonal) { described_class.new }
+    let(:board) { instance_double(Board, :grid => Array.new(7) { Array.new(6, nil) }) }
+
+    context 'player has 4 markers in a diagonal pattern' do
+      it 'returns true' do
+        expect(win_diagonal.check_diagonal_wins(board)).to eq true
       end
     end
   end
