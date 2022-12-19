a54a1ff1aaecfeacb333d2e794f8cf02dee1558e
Danny Neel <dneel10@gmail.com>
2022-12-19 12:54:59 -0500

    move driver file to main directory

--- lib/main.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'game'
-require_relative 'display'
-require_relative 'player'
-require_relative 'board'
-require_relative 'win_checker'
-
-Game.new.play_game
---
+++ main.rb
@@ -0,0 +1,7 @@
+require './lib/board'
+require './lib/display'
+require './lib/game'
+require './lib/player'
+require './lib/win_checker'
+
+Game.new.play_game
